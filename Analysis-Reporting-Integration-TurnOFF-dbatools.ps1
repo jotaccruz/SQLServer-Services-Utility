afb1105b4e48cacbf403d4e9e43dcaf78d2a07c8
JUAN CARLOS Cruz <juancs02011@gmail.com>
2019-10-08 14:04:01 -0600

    Adding functionalities
    Adding Disabling Reporting Services login

--- Analysis-Reporting-Integration-TurnOFF-dbatools.ps1
+++ Analysis-Reporting-Integration-TurnOFF-dbatools.ps1
@@ -3,9 +3,17 @@
 #Reporting Services
 #Integration Services
 #Disabling SQL Server Logins used by these services to connect to.
-#Requirements: dbatools has to be installed using "https://dbatools.io/"
+#Requirements: 
+#dbatools PS Module "https://dbatools.io/"
+#Run PS as Administrator
+#Grant to connect to SQL Server using Windows Authentication and sysadmin fixed server role
+#ReportServer-Login-Disable.sql script
 
 #Author: Juan Carlos Cruz#
+
+#Settings Variables
+#Directory where is located ReportServer-Login-Disable.sql
+$Location=""
 
 #Get current status#
 #Analysis, Reporting, Integration
@@ -19,3 +27,6 @@ Get-DbaService -Type SSAS,SSRS,SSIS | select -property ServiceName | Stop-Servic
 #Analysis, Reporting, Integration
 Get-DbaService -Type SSAS,SSRS,SSIS
 
+#Disabling Reporting Services Login
+Set-Location $Location
+sqlcmd.exe -i ReportServer-Login-Disable.sql
--- Analysis-Reporting-Integration-TurnOFF.ps1
+++ Analysis-Reporting-Integration-TurnOFF.ps1
@@ -1,11 +1,18 @@
-﻿#This script is to disable Extra Services installed within SQL Server
+﻿#To disable extra services installed within SQL Server
 #Analysis Services
 #Reporting Services
 #Integration Services
 #Disabling SQL Server Logins used by these services to connect to.
-#Requirements: dbatools has to be installed using "https://dbatools.io/"
+#Requirements: 
+#Run PS as Administrator
+#Grant to connect to SQL Server using Windows Authentication and sysadmin fixed server role
+#ReportServer-Login-Disable.sql script
 
 #Author: Juan Carlos Cruz#
+
+#Settings Variables
+#Directory where is located ReportServer-Login-Disable.sql
+$Location=""
 
 #Get current status#
 #Analysis
@@ -31,3 +38,6 @@ Get-Service |?{$_ -match 'ReportServer'} | select -property name,DisplayName,sta
 #Integration
 Get-Service |?{$_ -match 'MsDts'} | select -property name,DisplayName,starttype,status | ft -aut
 
+#Disabling Reporting Services Login
+Set-Location $Location
+sqlcmd.exe -i ReportServer-Login-Disable.sql
