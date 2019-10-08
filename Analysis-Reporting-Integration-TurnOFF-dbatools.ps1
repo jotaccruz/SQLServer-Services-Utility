56398ab951ed7d519f19157d32078cf6a36a2478
JUAN CARLOS Cruz <juancs02011@gmail.com>
2019-10-08 12:09:42 -0600

    Scripts to disable extra services using PS
    Scripts to disable SQL Server extra services such as Analysis, Reporting and Integration if you won't use them.

---
+++ Analysis-Reporting-Integration-TurnOFF-dbatools.ps1
@@ -0,0 +1,21 @@
+﻿#To disable extra services installed within SQL Server
+#Analysis Services
+#Reporting Services
+#Integration Services
+#Disabling SQL Server Logins used by these services to connect to.
+#Requirements: dbatools has to be installed using "https://dbatools.io/"
+
+#Author: Juan Carlos Cruz#
+
+#Get current status#
+#Analysis, Reporting, Integration
+Get-DbaService -Type SSAS,SSRS,SSIS
+
+# Disabling Services#
+#Analysis
+Get-DbaService -Type SSAS,SSRS,SSIS | select -property ServiceName | Stop-Service -passthru | Set-Service -startmode disabled
+
+#Get Final Status#
+#Analysis, Reporting, Integration
+Get-DbaService -Type SSAS,SSRS,SSIS
+
---
+++ Analysis-Reporting-Integration-TurnOFF.ps1
@@ -0,0 +1,33 @@
+﻿#This script is to disable Extra Services installed within SQL Server
+#Analysis Services
+#Reporting Services
+#Integration Services
+#Disabling SQL Server Logins used by these services to connect to.
+#Requirements: dbatools has to be installed using "https://dbatools.io/"
+
+#Author: Juan Carlos Cruz#
+
+#Get current status#
+#Analysis
+Get-Service |?{$_ -match 'OLAPService'} | select -property name,DisplayName,starttype,status | ft -aut
+#Reporting
+Get-Service |?{$_ -match 'ReportServer'} | select -property name,DisplayName,starttype,status | ft -aut
+#Integration
+Get-Service |?{$_ -match 'MsDts'} | select -property name,DisplayName,starttype,status | ft -aut
+
+# Disabling Services#
+#Analysis
+Get-Service | ?{$_ -match 'OLAPService'} | select -property name | Stop-Service -passthru | Set-Service -startmode disabled
+#Reporting
+Get-Service | ?{$_ -match 'ReportServer'} | select -property name | Stop-Service -passthru | Set-Service -startmode disabled
+#Integration
+Get-Service | ?{$_ -match 'MsDts'} | select -property name | Stop-Service -passthru | Set-Service -startmode disabled
+
+#Get Final Status#
+#Analysis
+Get-Service |?{$_ -match 'OLAPService'} | select -property name,DisplayName,starttype,status | ft -aut
+#Reporting
+Get-Service |?{$_ -match 'ReportServer'} | select -property name,DisplayName,starttype,status | ft -aut
+#Integration
+Get-Service |?{$_ -match 'MsDts'} | select -property name,DisplayName,starttype,status | ft -aut
+
